1a2ffc72c9c2540943ccba18f21fc8a847995ca9
tyler-cranmer <tyler.cranmer@outlook.com>
2021-11-24 17:49:26 -0800

    fixed sumsif formulas

--- data/sheets.py
+++ data/sheets.py
@@ -188,13 +188,13 @@ class MasterControls:
 
         #inserts titles for all each contributor
         def insert_title(owl_id,row_id):
-            title_data = [owl_id, f"=VLOOKUP(A{row_id},'Owl ID reference'!$A$2:$C$600,2,FALSE)", 'Contribution', 'Has this contribution been discussed with your WGL?','Link to Work', 'Other notes', 'Time contributed (Hours)', '# Functional area', 'Nominated WGL to review' ,'Product',
-            f'=sumifs($K$3:$K$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"F.Nest")', f'=sumifs($L$3:$L$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Product")', f'=sumifs(M$3:M$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"BD")',
-            f'=sumifs($N$3:$N$1142,$B$3:B$1142,B{row_id},H$3:$H$1000,"Creative & Design")', f'=sumifs($O$3:$O$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Dev/Engineering")', f'=sumifs($P$3:$P$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Growth")',
-            f'=sumifs(Q$3:Q$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Expenses")', f'=sumifs(R$3:R$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"MVI")', f'=sumifs(S$3:S$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Analytics")',
-            f'=sumifs(T$3:T$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Institutional Business")', f'=sumifs(U$3:U$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Talent, Ops & Change")', f'=sumifs(V$3:V$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"MetaGov")',
-            f'=sumifs(W$3:W$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Other")', f'=sumifs(X$3:X$1142,$B$3:$B$1142,B{row_id},H$3:$H$1000,"Lang-Ops")',f'=sumifs($Y$3:$Y$1000,$B$3:$B$1000,B{row_id},H$3:$H$1000,"Asia Pacific")',
-            f'=sumifs($Z$3:$Z$1000,$B$3:$B$1000,B{row_id},H$3:$H$1000,"Woman+Non-Binary")', f'=sumifs($AA$3:$AA$1000,$B$3:$B$1000,B{row_id},H$3:$H$1000,"Governance")', f'=sum(K{row_id}:AA{row_id})+AC{row_id + 1}', '', '', '',f'=(AB{row_id}/$B$1)+AD{row_id + 1}',
+            title_data = [owl_id, f"=VLOOKUP(A{row_id},'Owl ID reference'!$A$2:$B$600,2,FALSE)", 'Contribution', 'Has this contribution been discussed with your WGL?','Link to Work', 'Other notes', 'Time contributed (Hours)', '# Functional area', 'Nominated WGL to review' ,'Product',
+            f'=sumifs($K$3:$K$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"F.Nest")', f'=sumifs($L$3:$L$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Product")', f'=sumifs(M$3:M$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"BD")',
+            f'=sumifs($N$3:$N$1000,$B$3:B$1000,B{row_id},$H$3:$H$1000,"Creative & Design")', f'=sumifs($O$3:$O$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Dev/Engineering")', f'=sumifs($P$3:$P$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Growth&Marketing")',
+            f'=sumifs($Q$3:$Q$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Expenses")', f'=sumifs($R$3:$R$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"MVI")', f'=sumifs($S$3:$S$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Analytics")',
+            f'=sumifs($T$3:$T$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Institutional Business")', f'=sumifs($U$3:$U$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Talent, Ops & Change")', f'=sumifs($V$3:$V$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"MetaGov")',
+            f'=sumifs($W$3:$W$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Other")', f'=sumifs($X$3:$X$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Lang-Ops")',f'=sumifs($Y$3:$Y$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Asia Pacific")',
+            f'=sumifs($Z$3:$Z$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Woman+Non-Binary")', f'=sumifs($AA$3:$AA$1000,$B$3:$B$1000,B{row_id},$H$3:$H$1000,"Governance")', f'=sum(K{row_id}:AA{row_id})+AC{row_id + 1}', '', '', '',f'=(AB{row_id}/$B$1)+AD{row_id + 1}',
             f'=AE{row_id + 1}']
 
             ## WXY AC fixed USDC Stipen
